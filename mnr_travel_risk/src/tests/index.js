e7fb49db5d2adc2a8070f34563cdebc61c66e29a
Quinny-dev <katrynsteenkamp0@gmail.com>
2025-08-02 11:59:31 +0200

    payload

---
+++ mnr_travel_risk/src/tests/index.js
@@ -0,0 +1,11 @@
+function practiceTest(phrase) {
+  return phrase + "";
+}
+module.exports = {
+  practiceTest,
+}
+
+// function API() {
+//   return 'Real data from API';
+// }
+// module.exports =API;
